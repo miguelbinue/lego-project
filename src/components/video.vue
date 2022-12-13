f3200fa34d232cdfe9668855d5c41333bae74e81
clarajsanchez <clarajsanchez@hotmail.com>
2022-12-13 11:35:35 +0100

    video modify

---
+++ src/components/video.vue
@@ -0,0 +1,6 @@
+<template>
+    <video playsinline autoplay muted loop poster="../../assets/images/lego-game.180.png">
+        <source src="../../assets/videos/lego.day.mp4" type="video/mp4" id="source">
+          Your browser does not support the video tag.
+    </video>
+</template>
