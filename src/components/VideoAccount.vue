53aa7a14016f72361f6fa60d574d6660823aad19
miguelbinue <miguelbprada@gmail.com>
2022-12-15 23:10:35 +0100

    Finished the account comp

--- src/components/VideoAccount.vue
+++ src/components/VideoAccount.vue
@@ -1,6 +1,6 @@
 <template>
     <video playsinline autoplay muted loop poster="https://res.cloudinary.com/dbmjuz2tz/image/upload/v1671096619/account-background.png">
-        <source src="https://res.cloudinary.com/dbmjuz2tz/image/upload/v1671096619/account-background.png" type="video/mp4"  id="source"> 
+        <source src="https://res.cloudinary.com/dbmjuz2tz/video/upload/v1671140350/LEGO%20Playground/lego-account-background_xibwcj.mp4" type="video/mp4"  id="source"> 
           Your browser does not support the video tag.
     </video>
 </template>
