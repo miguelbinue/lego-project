c3158f4d06e1f365b5d691fe9eff8ed839c5f1a2
miguelbinue <miguelbprada@gmail.com>
2022-12-16 09:49:37 +0100

    finished responsive Desktop & Ipad

---
+++ src/components/VideoDark.vue
@@ -0,0 +1,6 @@
+<template>
+    <video playsinline autoplay muted loop poster="../../assets/images/lego-game.180.png">
+        <source src="https://res.cloudinary.com/dbmjuz2tz/video/upload/v1671030191/LEGO%20Playground/lego.night_awesat.mp4" type="video/mp4" id="source">
+          Your browser does not support the video tag.
+    </video>
+</template>
